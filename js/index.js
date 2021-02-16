905abe62eb52f9486187c0a305b3efe7e3f978c5
urevise <soocc15@gmail.com>
2021-02-16 20:41:00 +0900

    object sticker add

--- js/index.js
+++ js/index.js
@@ -14,4 +14,39 @@
         });
     });
 
+    const sticker = document.querySelectorAll('.sticker');
+    sticker.forEach(function(elem) {
+        elem.onmousedown = function(event) {
+            let shiftX = event.clientX - elem.getBoundingClientRect().left;
+            let shiftY = event.clientY - elem.getBoundingClientRect().top;    
+
+            elem.style.position = 'absolute';
+            elem.style.zIndex = 1000;
+
+            //document.body.append(elem);
+
+            function moveAt(pageX, pageY) {
+                elem.style.left = pageX - shiftX + 'px';
+                elem.style.top = pageY - shiftY + 'px';
+            }
+
+            moveAt(event.pageX, event.pageY);
+
+            function onMouseMove(event) {
+                moveAt(event.pageX, event.pageY);
+            }
+
+            document.addEventListener('mousemove', onMouseMove);
+
+            elem.onmouseup = function() {
+                document.removeEventListener('mousemove', onMouseMove);
+                elem.onmouseup = null;
+            }
+        }
+
+        elem.ondragstart = function() {
+            return false;
+        };
+    });
+
 })();
